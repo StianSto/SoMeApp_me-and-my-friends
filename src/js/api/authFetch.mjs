1eaa5f8f42cdf26ce8d30ab4276d611017bb93ef
Stian Stordal <stordalstian@gmail.com>
2022-11-22 19:46:29 +0100

    factory func for authorized fetch

---
+++ src/js/api/authFetch.mjs
@@ -0,0 +1,24 @@
+import { load } from "../storage/index.mjs"
+
+export function headers() {
+  const token = load("accessToken");
+  return {
+    "Content-Type": "application/json",
+    "Authorization": `Bearer ${token}`,
+  }
+}
+
+/**
+ * template for an authorized fetch request. 
+ * @param {string} apiUrl endpoint url to api.
+ * @param {*} options submit options for fetch. headers defaulted to Content-Type: application/json and Authorization: bearer token.
+ */
+export async function authFetch(apiUrl, options) {
+  return fetch(url, {
+    ...options, 
+    headers: headers(),
+  })
+}
+
+
+auth
