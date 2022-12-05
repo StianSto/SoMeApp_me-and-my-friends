0dd27774b1967a7bea9ae3b3be92a9ddde4c5dac
Stian Stordal <stordalstian@gmail.com>
2022-12-05 08:26:37 +0100

    rm logs

--- src/js/functions/filterByQuery.mjs
+++ src/js/functions/filterByQuery.mjs
@@ -2,15 +2,12 @@ export function filterByQuery(arr) {
   const params = new URLSearchParams(window.location.search);
   const search = params.get("search");
 
-  // if (!search) return arr;
+  if (!search) return arr;
 
-  console.log(search);
   const newArr = arr.filter(({ title, body, id }) => {
     if (title && title.includes(search)) return true;
     if (body && body.includes(search)) return true;
   });
-  console.log(newArr);
+
   return newArr;
-  // console.log(error);
-  // return arr;
 }
