515068fcf60cefe89ff5b7be55b8f5aacc2f5d73
Stian Stordal <stordalstian@gmail.com>
2022-12-09 12:50:38 +0100

    improved search

--- src/js/functions/filterByQuery.mjs
+++ src/js/functions/filterByQuery.mjs
@@ -10,13 +10,12 @@ export function filterByQuery(arr) {
       author: { name },
     } = post;
 
-    console.log(post);
-
     const searchFor = [title, body, name];
     const matches = searchFor.some((value) => {
-      if (!value || value.length == 0) return false;
+      if (!value || value.length < 1) return false;
       if (value.toLowerCase().includes(search.toLowerCase())) return true;
     });
+
     return matches ? post : false;
   });
 
