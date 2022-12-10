0af7673f7f748a3889acaedce875e0a180ea6de4
Stian Stordal <stordalstian@gmail.com>
2022-12-10 14:31:54 +0100

    js docs sign up + restructure of logic

--- src/js/api/auth/signupAuth.mjs
+++ src/js/api/auth/signupAuth.mjs
@@ -1,25 +1,23 @@
 import errMsg from "../../functions/errMsg.mjs";
 import login from "./loginAuth.mjs";
+import * as constants from "../constants.mjs";
+
+const APIUrl = constants.API_SOCIAL_URL + "/auth/register";
 
 /**
- * register user
- * @param {string} name no punctuation symbols except "_"
- * @param {string} email must be a valid noroff email
- * @param {string} password
- * @param {string} APIUrl
+ * register user to api. on succesful register it will attempt to login and redirect user to home page
+ * @param {Object} profileData object containing users name, email and password.
+ * @param {string} profileData.name name of user
+ * @param {string} profileData.email users email
+ * @param {string} profileData.password users password
  */
-
-export default async function signupAuth(name, email, password, APIUrl) {
+export default async function signupAuth(profileData) {
   const errMsgContainer = document.querySelector(".error-msg-signup");
 
   try {
     const response = await fetch(APIUrl, {
       method: "POST",
-      body: JSON.stringify({
-        name: name,
-        email: email,
-        password: password,
-      }),
+      body: JSON.stringify(profileData),
       headers: {
         "Content-type": "application/json; charset=UTF-8",
       },
@@ -27,10 +25,12 @@ export default async function signupAuth(name, email, password, APIUrl) {
     const result = await response.json();
     const responseError = result.errors;
 
-    console.log(response);
-    if (result.id) {
-      const profileData = { email, password };
-      login(profileData);
+    if (response.ok) {
+      const loginProfileData = {
+        email: profileData.email,
+        password: profileData.password,
+      };
+      login(loginProfileData);
     }
 
     errMsg(errMsgContainer, responseError);
