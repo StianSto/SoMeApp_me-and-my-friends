bc5fb8fcd99c550ff23da16a3e8171e033b8836a
Stian Stordal <stordalstian@gmail.com>
2022-12-17 22:43:17 +0100

    classes for theme

--- src/js/functions/setColorTheme.mjs
+++ src/js/functions/setColorTheme.mjs
@@ -1,9 +1,6 @@
 const root = document.querySelector(":root");
 export function setColorTheme(themeObject, el = root) {
   for (const [key, value] of Object.entries(themeObject)) {
-    console.log(`${key}`);
-    console.log(`${value}`);
-
     el.style.setProperty(key, value);
   }
 }
