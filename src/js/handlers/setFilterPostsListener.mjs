6e49aa19eac1b2f8d4d2097a9cffb1e8fbaeec01
StianSto <stordalstian@gmail.com>
2023-01-10 17:34:33 +0100

    bug: filter not appending results

--- src/js/handlers/setFilterPostsListener.mjs
+++ src/js/handlers/setFilterPostsListener.mjs
@@ -1,14 +1,12 @@
-import { getPost, getPosts } from "../api/posts/read.mjs";
+import { getPosts } from "../api/posts/read.mjs";
 import { getProfilePosts } from "../api/profiles/read.mjs";
-import createFlagString from "../functions/createFlagString.mjs";
-import * as templates from "../templates/index.mjs";
+import { postTemplate } from "../templates/index.mjs";
 
 export async function setFilterPostsListener() {
   const filterPostsContainer = document.querySelector("#filterPosts");
   filterPostsContainer.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    let flagstring;
     const postContainer = document.querySelector("#posts-wall");
     postContainer.innerHTML = "";
 
@@ -30,14 +28,17 @@ export async function setFilterPostsListener() {
           ? (result = await getProfilePosts(filterByAuthor, filterOptions))
           : (result = await getPosts(filterOptions));
         if (result.errors || !result) {
-          msg.innerHTML = `We found 0 results matching your filters: <br> ${result.errors[0].message}`;
+					msg.innerHTML = `We found 0 results matching your filters: <br> ${result.errors[0].message}`;
+					postContainer.replaceChildren();
         } else {
-          msg.innerText = `We found ${result.length} results matching your filters`;
+					msg.innerText = `We found ${result.length} results matching your filters`;
+					postContainer.replaceChildren(...result.map(postTemplate))
         }
       } catch (error) {
         console.log(error);
-      } finally {
-        postContainer.prepend(msg);
+			} finally {
+				console.log(result)
+				postContainer.prepend(msg);
       }
     })();
   });
