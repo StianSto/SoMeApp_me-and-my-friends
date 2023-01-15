e611894cacb7a8bf26db5a1bfcbef9516393d0bf
Stian Stordal <stordalstian@gmail.com>
2023-01-15 15:07:40 +0100

    editing gives user a simple confirmation

--- src/js/handlers/setEditPostListener.mjs
+++ src/js/handlers/setEditPostListener.mjs
@@ -1,9 +1,10 @@
 import * as posts from "../api/posts/update.mjs";
 import { convertTagsStringToArray } from "../functions/convertTagsToArray.mjs";
+import createModal from "../functions/createModal.mjs";
 export async function setEditPostListener() {
   const editPostForm = document.querySelector("#formEditPost");
 
-  editPostForm.addEventListener("submit", (event) => {
+  editPostForm.addEventListener("submit", async (event) => {
     event.preventDefault();
     const form = event.target;
 
@@ -17,6 +18,31 @@ export async function setEditPostListener() {
     editPostData.tags = tagsArray;
 
     console.log(editPostData);
-    posts.updatePost(editPostData);
+		const response = await posts.updatePost(editPostData);
+		console.log(response)
+		if (response.ok) {
+			const modal = createModal(modalElement)
+			modal.show()
+		}
   });
+}
+
+function modalElement() {
+
+	const modalElement = `
+	<div class="modal fade" id="modalCreatedPost" tabindex="-1" aria-labelledby="modalCreatedPost" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="modalCreatedPost">Modal title</h5>
+					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+				</div>
+				<div class="modal-body">
+					post was edited!
+				</div>
+			</div>
+		</div>
+	</div>
+	`
+	return modalElement
 }
